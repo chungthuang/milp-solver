1e649d5c60728901ab78153e007cdd5eaa793977
chungthuang <7826979+chungthuang@users.noreply.github.com>
2023-04-08 13:15:51 +0100

    List storage keys

--- lib/parachain-client/src/lib.rs
+++ lib/parachain-client/src/lib.rs
@@ -5,7 +5,7 @@ use jsonrpsee::{
     ws_client::{WsClient, WsClientBuilder},
 };
 use serde::{Deserialize, Serialize};
-//use codec::{Encode, Decode};
+use codec::{Encode, Decode};
 
 pub struct ParachainClient {
     rpc_client: WsClient,
@@ -33,4 +33,28 @@ impl ParachainClient {
             .await
             .map_err(|e| anyhow!("failed to get market state, err: {:?}", e))
     }
+
+    async fn get_storage_keys(&self, prefix: &str) -> Result<Vec<StorageKey>> {
+        const PER_PAGE: u32 = 100;
+        let encoded_prefix = prefix.encode();
+        let encoded_count = PER_PAGE.encode();
+        let mut all_keys = Vec::new();
+        let mut start_key: Option<StorageKey> = None;
+        loop {
+            let start_key_encoded: Option<Vec<u8>> = start_key.map(|k| k.encode());
+            let mut keys: Vec<StorageKey> = self.rpc_client
+                .request("state_getStorageKeysPaged", rpc_params![&encoded_prefix, &encoded_count, start_key_encoded])
+                .await
+                .map_err(|e| anyhow!("failed to get market state, err: {:?}", e))?;
+            if keys.is_empty() {
+                break
+            }
+            start_key = Some(keys[0].clone());
+            all_keys.append(&mut keys);
+        }
+        Ok(all_keys)
+    }
 }
+
+// https://docs.rs/sp-storage/latest/sp_storage/struct.StorageKey.html
+type StorageKey = Vec<u8>;
