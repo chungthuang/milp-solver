34d8dd3c9df5c9ce02830bfee9f43d9669f9a9fa
chungthuang <7826979+chungthuang@users.noreply.github.com>
2023-04-15 13:01:02 +0100

    cthuang: Form basic MILP problem

--- bin/server/src/main.rs
+++ bin/server/src/main.rs
@@ -1,6 +1,7 @@
-use log::info;
+use log::{error, info};
 use parachain_client::ParachainClient;
 use solver::Solver;
+use std::sync::Arc;
 use std::{env, time::Duration};
 
 #[tokio::main]
@@ -13,9 +14,14 @@ async fn main() {
         .parse()
         .expect("POLL_MARKET_FREQ_SECS is not u64");
     let rpc_client_url = env::var("RPC_CLIENT_URL").expect("RPC_CLIENT_URL not provided");
+    let parachain_client =
+        ParachainClient::new(&rpc_client_url).expect("failed to create parachain client");
+    let solver = Arc::new(Solver::new());
+    let cloned_solver = Arc::clone(&solver);
     handles.push(tokio::spawn(async move {
         tokio::spawn(poll_market_state(
-            rpc_client_url,
+            parachain_client,
+            cloned_solver,
             Duration::from_secs(poll_market_secs),
         ));
     }));
@@ -23,32 +29,42 @@ async fn main() {
         .expect("POLL_SOLUTION_FREQ_SECS not provided")
         .parse()
         .expect("POLL_SOLUTION_FREQ_SECS is not u64");
-    handles.push(tokio::spawn(poll_solution(Duration::from_secs(
-        poll_solution_secs,
-    ))));
+    handles.push(tokio::spawn(poll_solution(
+        solver,
+        Duration::from_secs(poll_solution_secs),
+    )));
 
     futures::future::join_all(handles).await;
 }
 
-async fn poll_market_state(rpc_client_url: String, poll_freq: Duration) {
+async fn poll_market_state(
+    parachain_client: ParachainClient,
+    solver: Arc<Solver>,
+    poll_freq: Duration,
+) {
     loop {
         info!("Poll market state");
-        let parachain_client = ParachainClient::new(&rpc_client_url)
-            .await
-            .expect("failed to create parachain client");
-        let state = parachain_client.get_market_state().await;
-        info!("market state {:?}", state);
+        match parachain_client.get_market_state().await {
+            Ok(state) => {
+                info!("market state {state:?}");
+                let problem_id = solver.submit(state.bids, state.asks);
+                info!("submit problem {problem_id:?}");
+            }
+            Err(err) => {
+                error!("failed to get market state {err}");
+            }
+        };
 
         tokio::time::sleep(poll_freq).await;
     }
 }
 
-async fn poll_solution(poll_freq: Duration) {
-    let solver = Solver::new();
+async fn poll_solution(solver: Arc<Solver>, poll_freq: Duration) {
     loop {
         info!("Poll market solution");
         tokio::time::sleep(poll_freq).await;
-        let problem_id = solver.submit();
-        info!("Problem ID {:?}", problem_id);
+        if let Err(e) = solver.poll() {
+            error!("failed to poll market solution {e:?}");
+        };
     }
 }
