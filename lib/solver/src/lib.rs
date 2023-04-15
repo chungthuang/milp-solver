805e7da52fddc696d06d69a517d14ad1417a3ced
chungthuang <7826979+chungthuang@users.noreply.github.com>
2023-04-15 13:01:02 +0100

    cthuang: Form basic MILP problem

--- lib/solver/src/lib.rs
+++ lib/solver/src/lib.rs
@@ -1,13 +1,14 @@
 pub mod error;
 
+use std::ops::Mul;
 use std::sync::{Arc, Mutex};
 
 use crate::error::Error;
 use good_lp::{
-    constraint,
     solvers::lp_solvers::{CbcSolver, LpSolver},
-    variables, SolverModel,
+    variable, Expression, ProblemVariables, SolverModel,
 };
+use parachain_client::Submission;
 use uuid::Uuid;
 
 pub struct Solver {
@@ -23,25 +24,54 @@ impl Solver {
         }
     }
 
-    pub fn submit(&self) -> Result<ProblemId, Error> {
-        variables! {
-            vars:
-                   a <= 1;
-              2 <= b <= 4;
-        }
+    pub fn submit(&self, bids: Vec<Submission>, asks: Vec<Submission>) -> Result<ProblemId, Error> {
+        let mut vars = ProblemVariables::new();
+
+        let bid_accepted = vars.add_vector(variable().binary(), bids.len());
+        let ask_accepted = vars.add_vector(variable().binary(), asks.len());
+
+        let bid_quantities = bids.iter().map(|(_, quantity, _)| *quantity as f64);
+        let ask_quantities = asks.iter().map(|(_, quantity, _)| *quantity as f64);
+
+        let bid_quantities: Vec<Expression> = bid_quantities.zip(bid_accepted.clone()).map(|(quantity, accepted)| accepted.mul(quantity)).collect();
+        let ask_quantities: Vec<Expression> = ask_quantities.zip(ask_accepted.clone()).map(|(quantity, accepted)| accepted.mul(quantity)).collect();
+
+        let bid_utilities = bids.iter().map(|(_, q, p)| (q * p) as f64);
+        let ask_costs = asks.iter().map(|(_, q, p)| (q * p) as f64);
+
+
+        let total_utility: Vec<Expression> = bid_utilities
+            .zip(bid_accepted)
+            .map(|(utility, accepted)| accepted.mul(utility))
+            .collect();
+        let total_costs: Vec<Expression> = ask_costs
+            .zip(ask_accepted)
+            .map(|(cost, accepted)| accepted.mul(cost))
+            .collect();
+
+        let social_welfare: Expression =
+            total_utility.into_iter().sum::<Expression>() - total_costs.into_iter().sum::<Expression>();
+
+        let quantity_match: Expression = bid_quantities.into_iter().sum::<Expression>() - ask_quantities.into_iter().sum::<Expression>();
+
         let problem_id = Uuid::new_v4();
         let cbc_solver = LpSolver(
             CbcSolver::new().with_temp_solution_file(format!("/tmp/milp_{}.sol", problem_id)),
         );
         let _ = vars
-            .maximise(10 * (a - b / 5) - b)
-            .using(cbc_solver) // multiple solvers available
-            .with(constraint!(a + 2 <= b))
-            .with(constraint!(1 + a >= 4 - b))
+            .maximise(social_welfare)
+            .using(cbc_solver)
+            .with(quantity_match.eq(0))
             .solve()
             .map_err(|err| Error::Solver(err.to_string()))?;
-        let mut active_problems = self.active_problems.lock().unwrap();
-        active_problems.push(problem_id);
+        self.active_problems.lock().unwrap().push(problem_id);
         Ok(problem_id)
+    }
+
+    pub fn poll(&self) -> Result<(), Error> {
+        for problem in self.active_problems.lock().unwrap().iter() {
+            println!("problem {problem}");
+        }
+        Ok(())
     }
 }
