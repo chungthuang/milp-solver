530fdd3f0d214f925a26db34944aad3fb5bcad19
chungthuang <7826979+chungthuang@users.noreply.github.com>
2023-04-16 10:10:23 +0100

    cthuang: Fix account ID deserialization

--- bin/server/src/main.rs
+++ bin/server/src/main.rs
@@ -12,8 +12,8 @@ async fn main() {
         .parse()
         .expect("POLL_MARKET_FREQ_SECS is not u64");
     let poll_freq = Duration::from_secs(poll_market_secs);
-    let rpc_client_url = env::var("RPC_CLIENT_URL").expect("RPC_CLIENT_URL not provided");
-    let parachain_client = ParachainClient::new(&rpc_client_url)
+    let rpc_client_addr = env::var("RPC_ADDRESS").expect("RPC_ADDRESS not provided");
+    let parachain_client = ParachainClient::new(&rpc_client_addr)
         .await
         .expect("failed to create parachain client");
 
